843524e653a1220f9a78dcda316ba16cb7ed15cd
ZhangGong <16069809@qq.com>
2019-07-10 12:57:46 +0800

    add index and bootstrap

--- src/main/java/org/muye/community/controller/IndexController.java
+++ src/main/java/org/muye/community/controller/IndexController.java
@@ -1,8 +1,16 @@
 package org.muye.community.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * @author Zz
  * create 2019--07--10--12:18
  **/
+@Controller
 public class IndexController {
+    @GetMapping({"/","index"})
+    public String index(){
+        return "index";
+    }
 }
