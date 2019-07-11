f6264b1021b2f4ab1e800f8e87f101ce9b99ddc1
ZhangGong <16069809@qq.com>
2019-07-11 12:05:11 +0800

    Change application.properties

--- src/main/java/org/muye/community/controller/AuthorizeController.java
+++ src/main/java/org/muye/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import org.muye.community.dto.AccessTokenDTO;
 import org.muye.community.dto.GithubUser;
 import org.muye.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,22 +17,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
     @Autowired
     GithubProvider githubProvider;
+//根据properties配置文件注入属性
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.redirect.uri}")
+    private String clientUri;
+    @Value("${github.client.secret}")
+    private String clientSecret;
 
-    @GetMapping("/callback")
+    @GetMapping(value = "/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO(
-                "74f355cbac8c0dca15a7",
-                "0e13f53e86a983bf80e88db2bcd8a5d5e457257f",
+                clientId,
+                clientSecret,
                 code,
-                "http://127.0.0.1:8080/callback",
+                clientUri,
                 state
         );
-//        accessTokenDTO.setCode(code);
-//        accessTokenDTO.setRedirect_uri("http://127.0.0.1:8080/callback");
-//        accessTokenDTO.setState(state);
-//        accessTokenDTO.setClient_id("74f355cbac8c0dca15a7");
-//        accessTokenDTO.setClient_secret("0e13f53e86a983bf80e88db2bcd8a5d5e457257f");
         String accessToke = githubProvider.getAccessToke(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToke);
         System.out.println(user);
--- src/main/java/org/muye/community/controller/IndexController.java
+++ src/main/java/org/muye/community/controller/IndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  **/
 @Controller
 public class IndexController {
-    @GetMapping({"/","index"})
+    @GetMapping(value = {"/","index"},name = "主页跳转")
     public String index(){
         return "index";
     }
